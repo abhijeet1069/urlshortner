5ce7b377169d7d9df4cc1a6cb716e2bf11c8279d
abhijeet1069 <abhijeet1069@gmail.com>
2026-09-16 06:22:19 +0530

    continue with work

--- src/main/java/com/backend/urlshortner/UrlshortnerApplication.java
+++ src/main/java/com/backend/urlshortner/UrlshortnerApplication.java
@@ -1,4 +1,4 @@
-package com.backend.urlshortner.core;
+package com.backend.urlshortner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/backend/urlshortner/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.backend.urlshortner.user.repository;
+
+import com.backend.urlshortner.user.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
